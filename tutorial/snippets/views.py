7b7a184ce795d67109bbff29499b0436515c3d2e
Matthew Hacker <hacker.matthew@gmail.com>
2022-01-09 16:56:44 -0500

    Replace explicit binding of viewsets to URLs with router

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,20 +1,11 @@
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
